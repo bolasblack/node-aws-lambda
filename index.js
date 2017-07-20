f5af28fa291d5946dc6a33563bd5f3bd466773dd
c4605 <bolasblack@gmail.com>
2017-07-20 23:55:25 +0800

    format

--- index.js
+++ index.js
@@ -1,168 +1,169 @@
-var fs = require('fs');
-var AWS = require('aws-sdk');
-var extend = require('util')._extend;
-var async = require('async');
+const fs = require('fs')
+const AWS = require('aws-sdk')
+const extend = require('util')._extend
+const async = require('async')
 
-exports.deploy = function(codePackage, config, callback, logger, lambda) {
+module.exports.deploy = (codePackage, config, callback, logger, lambda) => {
   if (!logger) {
-    logger = console.log;
+    logger = console.log
   }
 
-  if(!lambda) {
-    if("profile" in config) {
-      var credentials = new AWS.SharedIniFileCredentials({profile: config.profile});
-      AWS.config.credentials = credentials;
+  if (!lambda) {
+    if ("profile" in config) {
+      const credentials = new AWS.SharedIniFileCredentials({profile: config.profile})
+      AWS.config.credentials = credentials
     }
 
     if (process.env.HTTPS_PROXY) {
       if (!AWS.config.httpOptions) {
-        AWS.config.httpOptions = {};
+        AWS.config.httpOptions = {}
       }
-      var HttpsProxyAgent = require('https-proxy-agent');
-      AWS.config.httpOptions.agent = new HttpsProxyAgent(process.env.HTTPS_PROXY);
+      const HttpsProxyAgent = require('https-proxy-agent')
+      AWS.config.httpOptions.agent = new HttpsProxyAgent(process.env.HTTPS_PROXY)
     }
 
     lambda = new AWS.Lambda({
       region: config.region,
       accessKeyId: "accessKeyId" in config ? config.accessKeyId : "",
       secretAccessKey: "secretAccessKey" in config ? config.secretAccessKey : "",
-      sessionToken: "sessionToken" in config ? config.sessionToken : ""
-    });
+      sessionToken: "sessionToken" in config ? config.sessionToken : "",
+    })
   }
 
-  var params = {
+  const params = {
     FunctionName: config.functionName,
     Description: config.description,
     Handler: config.handler,
     Role: config.role,
     Timeout: config.timeout,
-    MemorySize: config.memorySize
-  };
-  if (config.vpc) params.VpcConfig = config.vpc;
-  var isPublish = (config.publish === true);
+    MemorySize: config.memorySize,
+  }
+  if (config.vpc) params.VpcConfig = config.vpc
+  const isPublish = (config.publish === true)
 
-  var updateEventSource = function(eventSource, callback) {
-    var params = extend({
+  const updateEventSource = (eventSource, callback) => {
+    const params = extend({
       FunctionName: config.functionName
-    }, eventSource);
+    }, eventSource)
 
     lambda.listEventSourceMappings({
       FunctionName: params.FunctionName,
-      EventSourceArn: params.EventSourceArn
-    }, function(err, data) {
+      EventSourceArn: params.EventSourceArn,
+    }, (err, data) => {
       if(err) {
-        logger("List event source mapping failed, please make sure you have permission");
-        callback(err);
+        logger("List event source mapping failed, please make sure you have permission")
+        callback(err)
       } else {
         if (data.EventSourceMappings.length === 0) {
-          lambda.createEventSourceMapping(params, function(err, data) {
+          lambda.createEventSourceMapping(params, (err, data) => {
             if(err) {
-              logger("Failed to create event source mapping!");
-              callback(err);
+              logger("Failed to create event source mapping!")
+              callback(err)
             } else {
-              callback();
+              callback()
             }
-          });
+          })
         } else {
-          async.eachSeries(data.EventSourceMappings, function(mapping, iteratorCallback) {
+          async.eachSeries(data.EventSourceMappings, (mapping, iteratorCallback) => {
             lambda.updateEventSourceMapping({
               UUID: mapping.UUID,
-              BatchSize: params.BatchSize
-            }, iteratorCallback);
-          }, function(err) {
-            if(err) {
-              logger("Update event source mapping failed");
-              callback(err);
+              BatchSize: params.BatchSize,
+            }, iteratorCallback)
+          }, err => {
+            if (err) {
+              logger("Update event source mapping failed")
+              callback(err)
             } else {
-              callback();
+              callback()
             }
-          });
+          })
         }
       }
-    });
-  };
+    })
+  }
 
-  var updateEventSources = function(callback) {
-    var eventSources;
+  const updateEventSources = callback => {
+    let eventSources
 
-    if(!config.eventSource) {
-      callback();
-      return;
+    if (!config.eventSource) {
+      callback()
+      return
     }
 
-    eventSources = Array.isArray(config.eventSource) ? config.eventSource : [ config.eventSource ];
+    eventSources = Array.isArray(config.eventSource) ? config.eventSource : [ config.eventSource ]
 
     async.eachSeries(
       eventSources,
       updateEventSource,
-      function(err) {
-        callback(err);
-      }
-    );
-  };
+      err => { callback(err) },
+    )
+  }
 
-  var updateFunction = function(callback) {
-    fs.readFile(codePackage, function(err, data) {
-      if(err) {
-        return callback('Error reading specified package "'+ codePackage + '"');
+  const updateFunction = callback => {
+    fs.readFile(codePackage, (err, data) => {
+      if (err) {
+        return callback('Error reading specified package "'+ codePackage + '"')
       }
 
-      lambda.updateFunctionCode({FunctionName: params.FunctionName, ZipFile: data, Publish: isPublish}, function(err, data) {
+      lambda.updateFunctionCode({
+        FunctionName: params.FunctionName,
+        ZipFile: data,
+        Publish: isPublish,
+      }, (err, data) => {
         if (err) {
-          var warning = 'Package upload failed. '
+          const warning = 'Package upload failed. '
           warning += 'Check your iam:PassRole permissions.'
-          logger(warning);
+          logger(warning)
           callback(err)
         } else {
-          lambda.updateFunctionConfiguration(params, function(err, data) {
+          lambda.updateFunctionConfiguration(params, (err, data) => {
             if (err) {
-              var warning = 'Update function configuration failed. '
-              logger(warning);
-              callback(err);
+              const warning = 'Update function configuration failed. '
+              logger(warning)
+              callback(err)
             } else {
-              updateEventSources(callback);
+              updateEventSources(callback)
             }
-          });
+          })
         }
-      });
-    });
-  };
+      })
+    })
+  }
 
-  var createFunction = function(callback) {
-    fs.readFile(codePackage, function(err, data) {
-      if(err) {
-        return callback('Error reading specified package "'+ codePackage + '"');
+  const createFunction = callback => {
+    fs.readFile(codePackage, (err, data) => {
+      if (err) {
+        return callback('Error reading specified package "'+ codePackage + '"')
       }
 
-      params['Code'] = { ZipFile: data };
-      params['Runtime'] = "runtime" in config ? config.runtime : "nodejs4.3";
-      params['Publish'] = isPublish;
-      lambda.createFunction(params, function(err, data) {
+      params['Code'] = { ZipFile: data }
+      params['Runtime'] = "runtime" in config ? config.runtime : "nodejs4.3"
+      params['Publish'] = isPublish
+      lambda.createFunction(params, (err, data) => {
         if (err) {
-          var warning = 'Create function failed. '
+          const warning = 'Create function failed. '
           warning += 'Check your iam:PassRole permissions.'
-          logger(warning);
+          logger(warning)
           callback(err)
         } else {
-          updateEventSources(callback);
+          updateEventSources(callback)
         }
-      });
-    });
-  };
+      })
+    })
+  }
 
-
-  lambda.getFunction({FunctionName: params.FunctionName}, function(err, data) {
+  lambda.getFunction({ FunctionName: params.FunctionName }, (err, data) => {
     if (err) {
       if (err.statusCode === 404) {
-        createFunction(callback);
+        createFunction(callback)
       } else {
-        var warning = 'AWS API request failed. '
+        const warning = 'AWS API request failed. '
         warning += 'Check your AWS credentials and permissions.'
-        logger(warning);
-        callback(err);
+        logger(warning)
+        callback(err)
       }
     } else {
-      updateFunction(callback);
+      updateFunction(callback)
     }
-  });
-};
+  })
+}
